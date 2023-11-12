9df6d6830b70c058fcb6b58549b93d98aa69d092
ermee <ermeeva.r@gmail.com>
2023-11-12 12:51:28 +0300

    Создан темплейт хостинга

---
+++ Host/Start/StartUpConfiguration.cs
@@ -0,0 +1,31 @@
+namespace Host.Start;
+
+/// <summary>
+/// Конфигурация билда веба
+/// </summary>
+public class StartUpConfiguration
+{
+    /// <summary>
+    /// Сконфгугрировать сервисы веба
+    /// </summary>
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddSwaggerGen();
+    }
+    
+    /// <summary>
+    /// Сконфигугрировать сборку веба
+    /// </summary>
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment environment, IServiceProvider serviceProvider)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
